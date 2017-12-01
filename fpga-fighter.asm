a2711c1ef309ea164579e2f0f797634e4b65c2e5
Marcus Vinícius <marcus.f22@gmail.com>
2017-12-01 16:30:29 -0200

    Created procedure printBKG.

--- fpga-fighter.asm
+++ fpga-fighter.asm
@@ -1,4 +1,4 @@
-.eqv SD_DATA_ADDR 	0x00813200
+.eqv SD_RYU_BKG_ADDR 	0x00813200
 .eqv VGA_INI_ADDR 	0xFF000000
 .eqv SRAM_ADDR 		0x10012000
 .eqv VGA_QTD_BYTE	76800
@@ -6,24 +6,26 @@
 .data
 .text
 main:
-    la      $a0, SD_DATA_ADDR
-    la      $a1, SRAM_ADDR
-    la      $a2, VGA_QTD_BYTE
-    li      $v0, 49
-    syscall
+	jal printBKG
+END: 	j END   
 
-# Copia pra SRAM
-    la      $t0, VGA_INI_ADDR
-    la      $t1, SRAM_ADDR
-    li      $t3, VGA_QTD_BYTE
-
-writeVGA:
-    lw      $t2, 0($t1)
-    sw      $t2, 0($t0)
-    addi    $t0, $t0, 4
-    addi    $t1, $t1, 4
-    addi    $t3, $t3, -4
-    slti    $t4, $t3, 1
-    beq     $t4, $zero, writeVGA
-
-end: j end
+printBKG: # Read background from SD to SRAM
+    	la $a0, SD_RYU_BKG_ADDR
+    	la $a1, SRAM_ADDR
+    	la $a2, VGA_QTD_BYTE
+    	li $v0, 49
+    	syscall
+writeVGA: # Read from SRAM and write to VGA - $a1 = SRAM_ADDR, $t0 = VGA_INI_ADDR
+	la $t0, VGA_INI_ADDR	# $t0 = VGA_INI_ADDR
+    	lw $t2, 0($a1)		# $t2 = SRAM[0]
+    	sw $t2, 0($t0)		# VGA[0] = $t2 = SRAM[0]
+    	
+	addi $t0, $t0, 4	# VGA++
+    	addi $a1, $a1, 4	# SRAM++
+    	addi $a2, $a2, -4	# QTD_BYTES--
+    	
+    	slti $t1, $a2, 1	# QTD_BYTES == 1 ? $t1 = 1 : $t1 = 0
+    	beq  $t1, $zero, writeVGA	# if $t1 == 0 then writeVGA
+    	
+    	# else return (when finished)
+    	jr $ra			 
